019458c5fd9ffeb753b8373812bdcecf6bd812b0
mainHossein <ho3eincheraghi20.06@gmail.com>
2026-07-24 23:34:00 +0330

    deleting unnecessary files

--- src/main/java/com/example/authservice/mapper/UserRoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.authservice.mapper;
-
-import com.example.authservice.database.entity.User;
-import com.example.authservice.dto.UserInfoDto;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring" , uses = {RoleStringMapper.class})
-public interface UserRoleMapper extends  BaseMapper<User, UserInfoDto> {
-}
