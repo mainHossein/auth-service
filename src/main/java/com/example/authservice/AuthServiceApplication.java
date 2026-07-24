8005c222b9da5a8972f1f3f59bcb8526275c41c4
mainHossein <ho3eincheraghi20.06@gmail.com>
2026-07-24 19:22:41 +0330

    writing a structured output in console for seeing server and swagger url

--- src/main/java/com/example/authservice/AuthServiceApplication.java
+++ src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -2,12 +2,25 @@ package com.example.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthServiceApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(AuthServiceApplication.class, args);
+        Integer port = run.getEnvironment().getProperty("server.port", Integer.class);
+        String contextPath = run.getEnvironment().getProperty("server.servlet.context-path", String.class);
+        printServerInfo(port, contextPath);
+    }
+
+    private static void printServerInfo(Integer port, String contextPath) {
+        System.out.printf("""
+                                  -------------------------------------------------------
+                                  | Server URL: http://localhost:%d                     |
+                                  | Swagger URL: http://localhost:%d%s/swagger-ui.html  |
+                                  -------------------------------------------------------
+                                  %n""", port, port, contextPath);
     }
 
 }
