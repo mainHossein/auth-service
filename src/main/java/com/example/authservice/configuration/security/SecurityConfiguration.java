6a4497a48746d68154d1c8eb86abafc3e45f0f48
Hossein Cheraghi <ho3eincheraghi20.06@gmail.com>
2026-07-26 07:59:54 +0330

    change argon parameters to spring defaults

--- src/main/java/com/example/authservice/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/example/authservice/configuration/security/SecurityConfiguration.java
@@ -26,10 +26,6 @@ public class SecurityConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new Argon2PasswordEncoder(16,
-                32,
-                1,
-                65536,
-                3);
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 }
