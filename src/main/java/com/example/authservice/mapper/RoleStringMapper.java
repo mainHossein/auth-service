aa24c427c688eaf6c8dd3c7ade98472846ef18f1
Hossein Cheraghi <ho3eincheraghi20.06@gmail.com>
2026-07-26 08:36:47 +0330

    fix the bug related to role mapping

--- src/main/java/com/example/authservice/mapper/RoleStringMapper.java
+++ src/main/java/com/example/authservice/mapper/RoleStringMapper.java
@@ -1,13 +1,16 @@
 package com.example.authservice.mapper;
 
 import com.example.authservice.database.entity.UserRoles;
+import com.example.authservice.database.enumerated.Role;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RoleStringMapper extends BaseMapper<UserRoles,String> {
     @Override
     default UserRoles toSource(String destination){
-        throw new UnsupportedOperationException();
+        return new UserRoles(Role.valueOf(destination.toUpperCase()));
     }
 
     @Override
