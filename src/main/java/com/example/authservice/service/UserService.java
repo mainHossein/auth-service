9a93452d4f5c06fc9e277af92d45e62fd6f37889
mainHossein <ho3eincheraghi20.06@gmail.com>
2026-07-28 23:06:45 +0330

    import transactional package

--- src/main/java/com/example/authservice/service/UserService.java
+++ src/main/java/com/example/authservice/service/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
