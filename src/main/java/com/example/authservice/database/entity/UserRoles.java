1b337d3699ef3a13d8c7e6afe6095be6a1c79ee0
Hossein Cheraghi <ho3eincheraghi20.06@gmail.com>
2026-07-28 16:06:22 +0330

    removing userId field in class

--- src/main/java/com/example/authservice/database/entity/UserRoles.java
+++ src/main/java/com/example/authservice/database/entity/UserRoles.java
@@ -3,6 +3,7 @@ package com.example.authservice.database.entity;
 import com.example.authservice.database.enumerated.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.UUID;
@@ -16,14 +17,13 @@ import java.util.UUID;
                 @UniqueConstraint(columnNames = {"fk_user_id", "role"})
         }
 )
+@NoArgsConstructor
 public class UserRoles extends AuditEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    @Column(name = "fk_user_id")
-    private UUID userId;
     @ManyToOne
-    @JoinColumn(name = "fk_user_id", updatable = false, insertable = false)
+    @JoinColumn(name = "fk_user_id" , nullable = false)
     private User user;
     @Enumerated(EnumType.STRING)
     private Role role;
