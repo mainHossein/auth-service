57c995a10830a065af823ae9695c06ebb2110aef
Hossein Cheraghi <ho3eincheraghi20.06@gmail.com>
2026-07-29 12:12:34 +0330

    removing extra dtos

--- src/main/java/com/example/authservice/mapper/UserUpdateMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.authservice.mapper;
-
-import com.example.authservice.database.entity.User;
-import com.example.authservice.dto.UserUpdateDto;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring", uses = {RoleStringMapper.class})
-public interface UserUpdateMapper extends BaseMapper<User, UserUpdateDto>{
-}
